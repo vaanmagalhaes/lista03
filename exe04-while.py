95eae9a7ecd6d4d15de0011241df484f5f37e623
Vanessa Magalhães <111777241+vaanmagalhaes@users.noreply.github.com>
2024-07-03 18:23:52 -0300

    Add files via upload

---
+++ exe04-while.py
@@ -0,0 +1,17 @@
+#Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3%
+#e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%.
+#Faça um programa que calcule e escreva o número de anos necessários para que a população do país A
+#ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
+
+populacao_a = 80000
+taxa_anualA = 3/100
+populacao_b = 200000
+taxa_anualB = 1.5/100
+anos = 0
+
+while populacao_a < populacao_b:
+    populacao_a += populacao_a * taxa_anualA
+    populacao_b += populacao_b * taxa_anualB
+    anos += 1
+    
+print('Serão necessários', anos, 'anos para que a população do país A ultrapasse ou iguale a população do país B.')
---
+++ exe05-for.py
@@ -0,0 +1,32 @@
+#Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais.
+#Valide a entrada e permita repetir a operação.
+
+#entrada
+
+populacao_a = float(input('Informe a população inicial do país A:'))
+taxa_anualA = float(input('Informe a taxa de crescimento anual do país A (%):'))
+populacao_b = float(input('Informe a população inicial do país B:'))
+taxa_anualB = float(input('Informe a taxa de crescimento anual do país B (%):'))
+
+while populacao_a == 0 or taxa_anualA == 0 or populacao_b == 0 or taxa_anualB == 0:
+    print('Informe um valor válido.')
+    populacao_a = float(input('Informe a população inicial do país A:'))
+    taxa_anualA = float(input('Informe a taxa de crescimento anual do país A (%):'))
+    populacao_b = float(input('Informe a população inicial do país B:'))
+    taxa_anualB = float(input('Informe a taxa de crescimento anual do país B (%):'))
+    
+anos = 0
+
+#processamento
+
+for i in range(100000):
+    if populacao_a < populacao_b:
+        populacao_a += populacao_a * taxa_anualA
+        populacao_b += populacao_b * taxa_anualB
+        anos += 1
+    elif populacao_a >= populacao_b:
+        break
+
+#saída
+
+print('Serão necessários', anos, 'anos para que a população do país A ultrapasse ou iguale a população do país B.')
